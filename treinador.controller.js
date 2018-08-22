ea61485b8c196766b3fb21954c3608d37d8eca2a
Regis <reagis@gmail.com>
2018-08-22 10:44:43 -0300

    Excluir treinador

--- treinador.controller.js
+++ treinador.controller.js
@@ -2,10 +2,21 @@ angular.module('treinadorPokemon')
     .controller('treinadorController',function(){
         var vm = this;
 
-        vm.treinadores = [{nome : 'Régis', cpf : '12345678900'}];
+        vm.treinadores = [
+            {nome : 'Régis', cpf : '12345678900'}
+        ];
 
         vm.cadastrarTreinador = function(treinador){
             vm.treinadores.push(treinador);
             delete vm.treinador;
+        }
+
+        vm.apagarTreinador = function(treinador){
+            var novosTreinadores;
+            novosTreinadores = vm.treinadores.filter(treinadorCorrente => {
+                return treinadorCorrente.nome !== treinador.nome;
+            });
+            
+            vm.treinadores = novosTreinadores;
         }
     });
